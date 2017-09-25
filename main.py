220a85b48fc7dccd61f98ab7e5d673413de811b2
Nikolaus Jones <njones1234567890@gmail.com>
2017-09-24 23:29:27 -0500

    Finish web caesar

--- main.py
+++ main.py
@@ -9,29 +9,29 @@ form = """
 <html>
     <head>
         <style>
-            form {
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }
-            textarea {
+        form {{
+            background-color: #eee;
+            padding: 20px;
+            margin: 0 auto;
+            width: 540px;
+            font: 16px sans-serif;
+            border-radius: 10px
+        }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
-        </style>
+            }}
+        </style>      
     </head>
     <body>
-        <form method=['POST']>
-            
-            <label>Rotate by:<input name="rot" type="test" value="0" /></label>
-            <textarea name="text"></textarea>
-            <input name="submit_message" type="submit" />
-            
-        </form>
+      <form method='post'>
+
+        <label>Rotate by:<input name="rot" type="test" value="0" /></label>
+        <textarea name="text">{0}</textarea>
+        <input name="submit message" type="submit" />
+
+      </form>
     </body>
 </html>
 """
@@ -42,10 +42,12 @@ def index():
     return form
 
 @app.route('/', methods=['POST'])
-def handle_encrypt():
-    rotnum = int(request.form["rot"])
-    message = str(request.form["text"])
+def encrypt():
+    rotnum = request.form.get("rot")
+    rotnum = int(rotnum)
+    message = request.form.get("text")
+    message = str(message)
     encrypted_string = str(rotate_string(message,rotnum))
-    return "<h1>" + encrypted_string + "</h1>"
+    return form.format(encrypted_string)
 
 app.run()
